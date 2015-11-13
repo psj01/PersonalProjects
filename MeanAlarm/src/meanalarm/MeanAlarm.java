a7be998b6a5f00000eefb70e13cee8450598d1c8
psj01 <psjobin@gmail.com>
2015-11-12 22:29:08 -0600

    update

--- MeanAlarm/src/meanalarm/MeanAlarm.java
+++ MeanAlarm/src/meanalarm/MeanAlarm.java
@@ -3,33 +3,174 @@
 package meanalarm;
 
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.PopupMenu;
+import java.awt.TextField;
 import java.awt.Toolkit;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.Time;
-import java.text.DateFormat;
-import java.text.Format;
-import java.text.ParseException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
-import sun.audio.AudioData;
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
-import sun.audio.ContinuousAudioDataStream;
+import java.util.HashMap;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  *
  * @author Jobin
  */
-public class MeanAlarm {
+
+
+
+public class MeanAlarm extends JFrame implements ActionListener  {    
     
     
+    private final JButton setAlarm = new JButton("SET ALARM");
+    
+    
+    JTextArea label1 = new JTextArea("Current time : ");
+    JTextField hr = new JTextField(2);
+    JTextArea semi1 = new JTextArea(":");
+    JTextField min = new JTextField(2);    
+    JTextArea semi2 = new JTextArea(":");
+    JTextField sec = new JTextField(2);
+    JRadioButton am = new JRadioButton("AM");
+    JRadioButton pm = new JRadioButton("PM");
+    
+    
+    JRadioButton am2 = new JRadioButton("AM");
+    JRadioButton pm2 = new JRadioButton("PM");
+    
+    JTextArea label2 = new JTextArea("ALARM WILL GO OFF IN : ");
+    
+    
+    JTextField ahr = new JTextField(2);
+    
+    JTextField amin = new JTextField(2);
+    
+    JTextField asec = new JTextField(2);
 
     
+    private MeanAlarm current = null;  
+    
+
+    public static void main(String[] args)
+    {
+        createAndShowGUI();
+    }
+
+    public static void createAndShowGUI() 
+    {
+        MeanAlarm frame1 = new MeanAlarm();
+        frame1.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame1.pack();
+        frame1.setSize(500, 500);
+        frame1.setVisible(true);
+        frame1.setResizable(false);
+    }
+
+    public MeanAlarm()
+    {
+        
+        super("WAKE UP YO!");
+        setLayout(new BorderLayout()); 
+        //current = new MeanAlarm(); 
+        
+        JPanel top = new JPanel();
+        top.setLayout(new FlowLayout());
+        hr.setEditable(false);
+        min.setEditable(false);
+        sec.setEditable(false);
+        label1.setEditable(false);
+        top.add(label1);
+        top.add(hr);
+        top.add(semi1);
+        top.add(min);
+        top.add(semi2);        
+        top.add(sec);
+        top.add(am);
+        top.add(pm);
+        add(top,BorderLayout.NORTH);
+        
+        
+        JPanel center = new JPanel();
+        
+        center.setLayout(new FlowLayout());
+        center.setBackground(Color.red);
+        
+        label2.setBackground(Color.red);
+        center.add(label2);
+        
+        add(center, BorderLayout.CENTER);
+        
+        JPanel south = new JPanel();
+        south.setLayout(new FlowLayout());
+        south.add(ahr);
+        south.add(amin);
+        south.add(am2);
+        south.add(pm2);
+        south.add(setAlarm);
+        
+        add(south, BorderLayout.SOUTH);
+        
+        
+    }
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        
+    }
+       
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+public class MeanAlarm extends JFrame {
+    
     public static void main(String[] args) throws IOException {
+        
+       
                 
        Date today = Calendar.getInstance().getTime(); 
       
@@ -120,3 +261,4 @@ public class MeanAlarm {
 }
 
 
+*/
